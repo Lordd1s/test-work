9869ec120ea75a858435811d92670a25f313f1ab
Lordd1s <uaudias@gmail.com>
2024-02-29 22:19:24 +0600

    included ready() func

--- training_system/apps.py
+++ training_system/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class TrainingSystemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'training_system'
+
+    def ready(self):
+        import training_system.signals
