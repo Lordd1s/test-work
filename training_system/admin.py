765009013382a558ebe4c61e403a55b65dc1bc18
Lordd1s <uaudias@gmail.com>
2024-02-29 22:18:46 +0600

    including admin panels (need upd on future)

--- training_system/admin.py
+++ training_system/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from training_system import models
+
 # Register your models here.
+admin.site.register(models.Product)
+admin.site.register(models.Lesson)
+admin.site.register(models.Groupp)
+admin.site.register(models.Subscription)
+
